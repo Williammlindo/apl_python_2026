1d67bb214abab2a79206a6c0a4fed15e84c8e92c
williammlindo <williammkm1@gmail.com>
2026-04-16 09:48:04 -0300

    Area do retangulo

--- Bemvindo.py
+++ Bemvindo.py
@@ -1,1 +1,6 @@
 print("Bem vindo")
+nome = 'williamlindo'
+idade = '16'
+print("Bem vindo",nome)
+print("Sua idade é:",idade)
+print("menu:\n1 comer \n2 ir embora\n3 dormi")
---
+++ Entrada.py
@@ -0,0 +1,5 @@
+Nome = input("Digite seu nome")
+Idade = input("Digite sua idade")
+Altura = input("Digite sua altura")
+
+print("Bem vindo",nome,"voce tem",idade,"anos e tem")
