88ec8291f44da679922180fa532981788a2546cc
williammlindo <williammkm1@gmail.com>
2026-05-07 09:36:55 -0300

    tarefa1

---
+++ areadoretangulo.py
@@ -0,0 +1,6 @@
+base = int(input ("Digite a base do retangulo (cm):") )
+altura = int(input ("Digite a altura do retangulo (cm):"))
+
+area = base * altura
+
+print(f"A area do retangulo é:{area} cm² ")
