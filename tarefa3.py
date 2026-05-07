dc89d7935eaff06f62f359083baa370238fc0afe
williammlindo <williammkm1@gmail.com>
2026-05-07 09:42:41 -0300

    tarefa3

---
+++ tarefa3.py
@@ -0,0 +1,10 @@
+from math import sqrt
+
+x1=float(input("Digite o valor de x1: "))
+x2=float(input("Digite o valor de x2: "))
+y1=float(input("Digite o valor de y1: "))
+y2=float(input("Digite o valor de y2: "))
+
+d= sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+print(f"A distancia entre os pontos é: {d:.2f}")
