8dcfd38bf8474f6fbdbc4e5e0dec7f47418b4321
williammlindo <williammkm1@gmail.com>
2026-05-08 11:52:30 -0300

    aula5

---
+++ caixa.py
@@ -0,0 +1,7 @@
+preco = float(input("Digite o valor: "))
+Quantidade = float(input("Digite a quantidade do produto: "))
+
+total = preco * Quantidade
+
+print (f"O resultado é {total}")
+
---
+++ login.py
@@ -0,0 +1,7 @@
+usuario = input ("usuario:")
+senha = input ("senha:")
+
+if (usuario == "procopio" and senha == "12345") or (usuario == "paiva" and senha == "123456"):
+    print("Seja bem vindo")
+else:
+    print("Usuario ou senha nao confere")
